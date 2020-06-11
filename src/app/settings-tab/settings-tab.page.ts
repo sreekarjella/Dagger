c0789148f5dc89e9883a38049dc937c4268cd552
sreekarjella <sreekarjella@gmail.com>
2020-06-11 12:13:20 +0530

    Back-up

--- src/app/settings-tab/settings-tab.page.ts
+++ src/app/settings-tab/settings-tab.page.ts
@@ -11,5 +11,26 @@ export class SettingsTabPage implements OnInit {
 
   ngOnInit() {
   }
+  // Delete this code.
+  onClick(event) {
+    const systemDark = window.matchMedia('(prefers-color-scheme: dark)');
+    systemDark.addEventListener('change', () => {
+      this.colorTest;
+    });
+    // systemDark.addListener(this.colorTest);
+    if (event.detail.checked) {
+      document.body.setAttribute('data-theme', 'dark');
+    } else {
+      document.body.setAttribute('data-theme', 'light');
+    }
+  }
+
+  colorTest(systemInitiatedDark) {
+    if (systemInitiatedDark.matches) {
+      document.body.setAttribute('data-theme', 'dark');
+    } else {
+      document.body.setAttribute('data-theme', 'light');
+    }
+  }
 
 }
